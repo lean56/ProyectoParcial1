00d3618d3ff6f870c4b4c647840d0fc6287b1b05
leonardo emil <theflaco56@hotmail.com>
2019-05-28 19:39:27 -0400

    Creando Entidad Producto

--- ProyectoParcial1/Entidades/Productos.cs
+++ ProyectoParcial1/Entidades/Productos.cs
@@ -1,12 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ProyectoParcial1.Entidades
 {
-   public class Productos
+    public class Productos
     {
+        public int ProductoId { get; set; }
+        public string Descripcion { get; set; }
+        public int Existencia { get; set; }
+        public int Costo { get; set; }
+        public int ValorInventario { get; set; }
+
+        public Productos()
+        {
+            ProductoId = 0;
+            Descripcion = string.Empty;
+            Existencia = 0;
+            Costo = 0;
+            ValorInventario = 0;
+        }
     }
 }
